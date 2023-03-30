9f8fb15dd4774803b4339156b4621a0394fa2f51
Soumyo Chakravorty <92665254+soumyo2002@users.noreply.github.com>
2023-03-30 17:48:26 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,27 @@
+/*Write a program in C++ that creates a class parcel containing private member variable weight
+(of the parcel), protected variable shipping_cost, write a parameterized constructor that
+initializes these variables. Write a member function with name Cal_shipping () to calculate the
+shipping cost according to formula Rs. 10/Kg. Create a class called Box, which is publicly
+inheriting Parcel class. It should have private member variable no_of_boxes which will be
+initialized using a constructor based on the weight data member of parcel class( if weight
+exceeds 30 kg then no.of boxes requires 2 and if exceeds 50 kg no.of boxes required 5,
+otherwise 1 box required). Override class Cal_shipping () function defined in the class Parcel
+and add Rs. 30 to the shipping_cost variable if the volume of the box exceeds 3 units. Display all
+the information with the final shipping cost.*/
+
+#include<iostream>
+using namespace std;
+class parcel
+{
+    public:
+    int 10=10;
+
+};
+class B{
+    public:
+int a=10;
+};
+int main(){
+    B ob;
+   return 0;
+}
